1fe934b3e3a50935b48cdbee2726dd18e5389efe
Lasha Giorgi Qutateladze <lashagiorgi07@gmail.com>
2023-10-23 20:06:52 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import keyConceptsImage from './assets/images/key-concepts.png';
+import componentsImage from './assets/images/components.png';
+import stateImage from './assets/images/state.png';
+import eventsImage from './assets/images/events.png';
+
+const concepts = [
+  {
+    title: 'Components',
+    image: componentsImage,
+    description:
+      'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
+  },
+  {
+    title: 'State',
+    image: stateImage,
+    description:
+      'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state, and multiple components can share state.',
+  },
+  {
+    title: 'Events',
+    image: eventsImage,
+    description:
+      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions get executed for which event.',
+  },
+];
+
+function Concept(props) {
+  return (
+    <li className="concept">
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src={keyConceptsImage} alt="Medal badge with a star" />
+        <h1>Key React Concepts</h1>
+        <p>Selected key React concepts you should know about</p>
+      </header>
+      <ul id="concepts">
+        {concepts.map((concept, index) => (
+          <Concept
+            key={index}
+            title={concept.title}
+            image={concept.image}
+            description={concept.description}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
